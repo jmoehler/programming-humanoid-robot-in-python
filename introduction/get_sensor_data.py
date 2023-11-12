c07934c7f165b6a3782817514cc404f8236d96c8
j.moehler <j.moehler@campus.tu-berlin.de>
2023-11-12 14:23:25 +0100

    some changes

--- introduction/get_sensor_data.py
+++ introduction/get_sensor_data.py
@@ -8,6 +8,9 @@ In this exercise you need to know how to get sensor data.
 '''
 
 # add PYTHONPATH
+
+
+
 import os
 import sys
 sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'software_installation'))
@@ -19,8 +22,12 @@ class MyAgent(SparkAgent):
     def think(self, perception):
         angle = 0
         temperature = 0
+        angle = perception.joint['HeadYaw']
+        temperature = perception.joint_temperature['HeadYaw']
+
         # YOUR CODE HERE
         # get angle and temperature to current data of joint HeadYaw
+
 
         print('HeadYaw angle: ' + str(angle) + ' temperature: ' + str(temperature))
         return super(MyAgent, self).think(perception)

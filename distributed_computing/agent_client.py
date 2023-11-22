eea315ffb61f2d3cb82fc8e1de6d63f7fb48c521
j.moehler <j.moehler@campus.tu-berlin.de>
2023-11-22 14:45:44 +0100

    Refactor agent_client.py and agent_server.py to provide RPC services

--- distributed_computing/agent_client.py
+++ distributed_computing/agent_client.py
@@ -9,6 +9,7 @@
  * set_transform
 * You can test RPC server with ipython before implementing agent_client.py
 """
+
 import os
 import sys
 sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'kinematics'))
@@ -18,35 +19,42 @@ import threading
 import pickle
 from xmlrpc.server import SimpleXMLRPCServer, SimpleXMLRPCRequestHandler
 
-
+# Define a custom request handler for the XML-RPC server
 class RequestHandler(SimpleXMLRPCRequestHandler):
     rpc_paths = ('/RPC2',)
 
-
+# ServerAgent extends InverseKinematicsAgent and provides RPC services
 class ServerAgent(InverseKinematicsAgent):
     '''ServerAgent provides RPC service
     '''
 
     def __init__(self):
         super(ServerAgent, self).__init__()
+
+        # Load posture classifier from a pickle file
         robot_pose_data = open(r'..\joint_control\robot_pose.pkl', 'rb')
-        self.posture_classifier = pickle.load(robot_pose_data)  # LOAD YOUR CLASSIFIER
+        self.posture_classifier = pickle.load(robot_pose_data)
         self.posture = 'unknown'
         robot_pose_data.close()
-        # create server
+
+        # Create an XML-RPC server on localhost:6666
         self.server = SimpleXMLRPCServer(('localhost', 6666), requestHandler=RequestHandler, allow_none=True)
         self.server.register_introspection_functions()
         self.server.register_multicall_functions()
         self.server.register_instance(self)
+
+        # Start the server in a separate thread
         self.thread = threading.Thread(target=self.server.serve_forever)
         self.thread.start()
         print('starting server ...')
 
     def think(self, perception):
+        # Recognize the current posture based on perception data
         self.posture = self.recognize_posture(perception)
         return super(ServerAgent, self).think(perception)
 
     def recognize_posture(self, perception):
+        # Use a posture classifier to determine the current posture
         posture = 'unknown'
         postures = ['HeadBack', 'Left', 'Right', 'Crouch', 'Knee', 'Stand', 'Sit', 'StandInit', 'Frog', 'Back', 'Belly']
         joint_names = ['LHipYawPitch', 'LHipRoll', 'LHipPitch', 'LKneePitch', 'RHipYawPitch', 'RHipRoll', 'RHipPitch',
@@ -73,8 +81,8 @@ class ServerAgent(InverseKinematicsAgent):
         return str(self.posture)
 
     def execute_keyframes(self, keyframes):
-        """excute keyframes, note this function is blocking call,
-        e.g. return until keyframes are executed
+        """execute keyframes, note this function is a blocking call,
+        e.g. it returns only after keyframes are executed
         """
         # YOUR CODE HERE
         self.keyframes = keyframes
@@ -85,11 +93,11 @@ class ServerAgent(InverseKinematicsAgent):
         return self.transforms[name]
 
     def set_transform(self, effector_name, transform):
-        """solve the inverse kinematics and control joints use the results
+        """solve the inverse kinematics and control joints using the results
         """
         self.transforms[effector_name] = transform
 
-
 if __name__ == '__main__':
+    # Create an instance of the ServerAgent and start running
     agent = ServerAgent()
     agent.run()
--- distributed_computing/agent_server.py
+++ distributed_computing/agent_server.py
@@ -1,60 +1,107 @@
-'''In this file you need to implement remote procedure call (RPC) server
+"""In this file, you need to implement a remote procedure call (RPC) server.
 
-* There are different RPC libraries for python, such as xmlrpclib, json-rpc. You are free to choose.
+* There are different RPC libraries for Python, such as xmlrpclib, json-rpc. You are free to choose.
 * The following functions have to be implemented and exported:
- * get_angle
- * set_angle
- * get_posture
- * execute_keyframes
- * get_transform
- * set_transform
-* You can test RPC server with ipython before implementing agent_client.py
-'''
+  * get_angle
+  * set_angle
+  * get_posture
+  * execute_keyframes
+  * get_transform
+  * set_transform
+* You can test the RPC server with IPython before implementing agent_client.py
+"""
 
-# add PYTHONPATH
 import os
 import sys
 sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'kinematics'))
 
-from inverse_kinematics import InverseKinematicsAgent
+from kinematics.inverse_kinematics import InverseKinematicsAgent
+import threading
+import pickle
+from xmlrpc.server import SimpleXMLRPCServer, SimpleXMLRPCRequestHandler
 
+# Define a custom request handler for the XML-RPC server
+class RequestHandler(SimpleXMLRPCRequestHandler):
+    rpc_paths = ('/RPC2',)
 
+# ServerAgent extends InverseKinematicsAgent and provides RPC services
 class ServerAgent(InverseKinematicsAgent):
     '''ServerAgent provides RPC service
     '''
-    # YOUR CODE HERE
-    
-    
+
+    def __init__(self):
+        super(ServerAgent, self).__init__()
+
+        # Load posture classifier from a pickle file
+        robot_pose_data = open(r'..\joint_control\robot_pose.pkl', 'rb')
+        self.posture_classifier = pickle.load(robot_pose_data)
+        self.posture = 'unknown'
+        robot_pose_data.close()
+
+        # Create an XML-RPC server on localhost:6666
+        self.server = SimpleXMLRPCServer(('localhost', 6666), requestHandler=RequestHandler, allow_none=True)
+        self.server.register_introspection_functions()
+        self.server.register_multicall_functions()
+        self.server.register_instance(self)
+
+        # Start the server in a separate thread
+        self.thread = threading.Thread(target=self.server.serve_forever)
+        self.thread.start()
+        print('starting server ...')
+
+    # Recognize the current posture
+    def think(self, perception):
+        # Recognize the current posture based on perception data
+        self.posture = self.recognize_posture(perception)
+        return super(ServerAgent, self).think(perception)
+
+    def recognize_posture(self, perception):
+        # Use a posture classifier to determine the current posture
+        posture = 'unknown'
+        postures = ['HeadBack', 'Left', 'Right', 'Crouch', 'Knee', 'Stand', 'Sit', 'StandInit', 'Frog', 'Back', 'Belly']
+        joint_names = ['LHipYawPitch', 'LHipRoll', 'LHipPitch', 'LKneePitch', 'RHipYawPitch', 'RHipRoll', 'RHipPitch',
+                       'RKneePitch']
+        values = []
+        for joint_name in joint_names:
+            values.append(perception.joint[joint_name])
+        values += perception.imu
+        ID = self.posture_classifier.predict([values])[0]
+        posture = postures[ID]
+        return posture
+
     def get_angle(self, joint_name):
-        '''get sensor value of given joint'''
-        # YOUR CODE HERE
-    
+        # Get the angle of the joint from perception
+        """Get the sensor value of the given joint."""
+        return self.perception.joint.get(joint_name)
+
     def set_angle(self, joint_name, angle):
-        '''set target angle of joint for PID controller
-        '''
-        # YOUR CODE HERE
+        # Set the target angle of the joint for PID controller
+        """Set the target angle of the joint for PID controller."""
+        self.target_joints[joint_name] = angle
 
     def get_posture(self):
-        '''return current posture of robot'''
-        # YOUR CODE HERE
+        # Return the current posture of the robot
+        """Return the current posture of the robot."""
+        return str(self.posture)
 
     def execute_keyframes(self, keyframes):
-        '''excute keyframes, note this function is blocking call,
-        e.g. return until keyframes are executed
-        '''
+        # Execute keyframes
+        """Execute keyframes. Note that this function is a blocking call,
+        e.g., it returns only after keyframes are executed."""
         # YOUR CODE HERE
+        self.keyframes = keyframes
 
     def get_transform(self, name):
-        '''get transform with given name
-        '''
-        # YOUR CODE HERE
+        # Get the transform with the given name
+        """Get the transform with the given name."""
+        return self.transforms[name]
 
     def set_transform(self, effector_name, transform):
-        '''solve the inverse kinematics and control joints use the results
-        '''
-        # YOUR CODE HERE
+        # Solve the inverse kinematics and control joints using the results
+        """Solve the inverse kinematics and control joints using the results."""
+        self.transforms[effector_name] = transform
 
 if __name__ == '__main__':
+    # Create an instance of the ServerAgent and start running
     agent = ServerAgent()
     agent.run()
-
